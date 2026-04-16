72378afbfd2d31f9ec554dac0a968989af158c08
Laurynas Blankas <laurynas.blankas@gmail.com>
2026-04-16 12:16:06 +0300

    Initial commit

---
+++ Water_ping.py
@@ -0,0 +1,35 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
+api_key = os.getenv("api_key")
+receiver_id = os.getenv("receiver_id")
+
+def send_telegram(message):
+    url_telegram = f"https://api.telegram.org/bot{api_key}/sendMessage"
+    payload = {"chat_id": receiver_id, "text": message}
+    requests.post(url_telegram, json=payload)
+
+messages = ["Don't forget to hydrate! 💧",
+            "Time for a water break! 🚰",
+            "Stay hydrated! 🥤",
+            "Drink some water! 💦",
+            "Water is life! 🌊",
+            "Water is love! 🌊",
+            "Drink up! 🥛",
+            "Keep sipping! 🍶",
+            "Water is essential! 💧",
+            "Hydration is key! 🔑",
+            "Water is the best! 💧",
+            "Don't forget to drink water! 🚰",
+            "Stay refreshed! 🥤",
+            "Water is your friend! 💦",
+            "Keep hydrated! 🥛",
+            "Don't forget to drink water! 🚰",
+            "Stay hydrated and healthy! 🥤"]
+msg = messages[random.randint(0, len(messages)-1)]
+
+send_telegram(msg)
